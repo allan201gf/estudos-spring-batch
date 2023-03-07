5c62735cada69868a984fe39d8aef42ff080fef9
allan201gf <allan201gf@gmail.com>
2023-03-07 20:26:20 -0300

    Ola mundo no Spring Batch

---
+++ firstProjectSpringBatch/src/test/java/br/com/allangf/firstProjectSpringBatch/FirstProjectSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allangf.firstProjectSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
