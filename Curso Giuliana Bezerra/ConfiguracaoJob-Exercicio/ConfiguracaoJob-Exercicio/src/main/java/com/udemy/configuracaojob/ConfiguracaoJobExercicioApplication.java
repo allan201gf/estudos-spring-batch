5b005527c47a8f9eb7140fcc423f46e59ea41098
allan201gf <allan201gf@gmail.com>
2023-03-13 21:12:06 -0300

    Atividade aula

---
+++ Curso Giuliana Bezerra/ConfiguracaoJob-Exercicio/ConfiguracaoJob-Exercicio/src/main/java/com/udemy/configuracaojob/ConfiguracaoJobExercicioApplication.java
@@ -0,0 +1,14 @@
+package com.udemy.configuracaojob;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracaoJobExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracaoJobExercicioApplication.class, args);
+	}
+
+}
